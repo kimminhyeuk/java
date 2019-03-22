5be6be190a1f3f46cc209da75bed2cf04bd661ca
USER <USER@AMP-05>
2019-03-22 11:49:31 +0900

    a

---
+++ chap04_ex06/src/chap04_ex06/Circle.java
@@ -0,0 +1,20 @@
+package chap04_ex06;
+
+public class Circle {
+	int radius; 
+
+	 
+	public Circle(int radius) { 
+		this.radius = radius; 
+	
+	 
+	} 
+	 public double getArea() { 
+		return 3.14 * radius * radius;
+		
+		
+	 }
+
+	 
+
+}
---
+++ chap04_ex06/src/chap04_ex06/CircleArray.java
@@ -0,0 +1,15 @@
+package chap04_ex06;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class CircleArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public Circle(i
+			
+		}
+
+	}
+
+}
